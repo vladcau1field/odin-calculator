2169bf6d02d15b5bbe9ffb57ead0e9b771fd79e1
vladcaulfield <vladkryzhenkov8@gmail.com>
2025-02-10 23:07:03 +0300

    feat: add operate function logic and variables

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(numberA, numberB) {
+  return numberA + numberB;
+}
+
+function subsctract(numberA, numberB) {
+  return numberA - numberB;
+}
+
+function multiply(numberA, numberB) {
+  return numberA * numberB;
+}
+
+function divide(numberA, numberB) {
+  return numberA / numberB;
+}
+
+let firstNumber;
+let operator;
+let anotherNumber;
+
+function operate(firstNumber, operator, anotherNumber) {
+  if (operator == "+") {
+    return add(firstNumber, anotherNumber);
+  } else if (operator == "-") {
+    return subsctract(firstNumber, anotherNumber);
+  } else if (operator == "*") {
+    return multiply(firstNumber, anotherNumber);
+  } else if (operator == "/") {
+    return divide(firstNumber, anotherNumber);
+  }
+}
