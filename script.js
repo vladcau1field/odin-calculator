006511b2d0990f506da847eb005481f4ac2b81a2
vladcaulfield <vladkryzhenkov8@gmail.com>
2025-02-12 19:00:49 +0300

    feat: add vars for numbers

--- script.js
+++ script.js
@@ -31,5 +31,14 @@ function operate(firstNumber, operator, anotherNumber) {
 }
 
 const displayDigits = document.querySelector("#display");
-
+const one = document.querySelector("#1");
+const two = document.querySelector("#2");
+const three = document.querySelector("#3");
+const four = document.querySelector("#4");
+const five = document.querySelector("#5");
+const six = document.querySelector("#6");
+const seven = document.querySelector("#7");
+const eight = document.querySelector("#8");
+const nine = document.querySelector("#9");
+const zero = document.querySelector("#0");
 displayDigits.textContent = "hello";
