cca2b6788e803205db8e36beb0f4aa04fde1e148
vladcaulfield <vladkryzhenkov8@gmail.com>
2025-02-15 15:40:41 +0300

    refactor: optimize all button's logic

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function divide(numberA, numberB) {
 let firstNumber = null;
 let operator = null;
 let anotherNumber = null;
+let resetScreen = false;
 
 function operate(firstNumber, operator, anotherNumber) {
   switch (operator) {
@@ -38,7 +39,6 @@ function operate(firstNumber, operator, anotherNumber) {
 
 const displayDigits = document.querySelector("#display");
 const numbers = document.querySelectorAll(".digits");
-const currentDigits = document.querySelector("#currentDigits");
 const addBtn = document.querySelector("#add");
 const equalsBtn = document.querySelector("#equals");
 const clearBtn = document.querySelector("#clear");
@@ -49,28 +49,47 @@ const divideBtn = document.querySelector("#divide");
 // numbers
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
+    if (resetScreen) {
+      displayDigits.textContent = "";
+      resetScreen = false;
+    }
     if (displayDigits.textContent.length < 10) {
       displayDigits.textContent += number.textContent;
     }
   });
 });
 
-// +
-addBtn.addEventListener("click", () => {
-  if (displayDigits.textContent != "") {
+function setOperator(newOperator) {
+  if (operator !== null && !resetScreen) {
+    anotherNumber = parseFloat(displayDigits.textContent);
+    firstNumber = operate(firstNumber, operator, anotherNumber);
+    displayDigits.textContent = parseFloat(firstNumber.toFixed(5));
+  } else {
     firstNumber = parseFloat(displayDigits.textContent);
-    operator = "+";
-    displayDigits.textContent = "";
   }
-});
+
+  operator = newOperator;
+  resetScreen = true;
+}
+
+addBtn.addEventListener("click", () => setOperator("+"));
+subtrBtn.addEventListener("click", () => setOperator("-"));
+multiplyBtn.addEventListener("click", () => setOperator("*"));
+divideBtn.addEventListener("click", () => setOperator("/"));
 
 equalsBtn.addEventListener("click", () => {
-  if (firstNumber != null && operator != null) {
+  if (firstNumber !== null && operator !== null) {
     anotherNumber = parseFloat(displayDigits.textContent);
     let result = operate(firstNumber, operator, anotherNumber);
-    displayDigits.textContent = result;
-    firstNumber = result;
+    if (result === "wtf?") {
+      displayDigits.textContent = result;
+      firstNumber = null;
+    } else {
+      displayDigits.textContent = parseFloat(result.toFixed(5));
+      firstNumber = result;
+    }
     operator = null;
+    resetScreen = true;
   }
 });
 
@@ -79,28 +98,5 @@ clearBtn.addEventListener("click", () => {
   firstNumber = null;
   operator = null;
   anotherNumber = null;
-});
-
-subtrBtn.addEventListener("click", () => {
-  if (displayDigits.textContent != "") {
-    firstNumber = parseFloat(displayDigits.textContent);
-    operator = "-";
-    displayDigits.textContent = "";
-  }
-});
-
-multiplyBtn.addEventListener("click", () => {
-  if (displayDigits.textContent != "") {
-    firstNumber = parseFloat(displayDigits.textContent);
-    operator = "*";
-    displayDigits.textContent = "";
-  }
-});
-
-divideBtn.addEventListener("click", () => {
-  if (displayDigits.textContent != "") {
-    firstNumber = parseFloat(displayDigits.textContent);
-    operator = "/";
-    displayDigits.textContent = "";
-  }
+  resetScreen = false;
 });
